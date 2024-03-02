0d3ad77b4e3c235a22a384b5ecfb0bbbcb52b750
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-03-02 16:23:49 +0100

    delete load

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,19 @@ import Sidenav from './components/Sidenav';
 import Accueil from './pages/Accueil';
 import Apropos from './pages/APropos';
 import Projets from './pages/Projets';
-import Loader from './components/Loader';
 
 
 const App = () => {
-  const [loading, setLoading] = useState(true); 
 
-  useEffect(() => {
-    setTimeout(() => {
-      
-      setLoading(false); 
-    }, 5000); 
-  });
 
 
   return (
       <div>        
-        {loading && <Loader />} 
-        <div className={`content ${loading ? "hidden" : ""}`}>
+
         <Sidenav />
         <Accueil />
         <Apropos />
         <Projets />
-      </div>
       </div>
 
   );
