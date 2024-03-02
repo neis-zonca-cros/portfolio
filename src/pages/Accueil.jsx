e2cd1fc15226792f7f3a86b3f21cb2225b764b62
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-03-02 23:19:33 +0100

    spline

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -20,9 +20,8 @@ const Accueil = () => {
         <div className="absolute opacity-20 font-[dream] text-4xl sm:text-4xl md:text-6xl lg:text-6xl rotate-0 md:-rotate-90 lg:-rotate-90 sm:rotate-0 top-20 left-1/2 transform -translate-x-1/2 md:top-1/2 lg:top-1/2 md:left-20 lg:left-20">
           PORTFOLIO
         </div >
+        <Spline scene="https://prod.spline.design/q9KgjBw7qRiINVp5/scene.splinecode" />
 
-
-        <Spline scene="https://prod.spline.design/jncBDsruSEvD4UU8/scene.splinecode" />
         </div>
     </section>
   );
