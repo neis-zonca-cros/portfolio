6508cb49d24503ab3e77dbaeb29b95d03df1e240
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-03-03 11:19:56 +0100

    clock

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 import bgacc from '../assets/bgacc.jpg';
+import loader from '../assets/loader.svg';
 
 
 
@@ -17,11 +18,19 @@ const Accueil = () => {
           backgroundRepeat: 'no-repeat',
           display: 'flex',
         }}>
-        <div className="absolute opacity-20 font-[dream] text-4xl sm:text-4xl md:text-6xl lg:text-6xl rotate-0 md:-rotate-90 lg:-rotate-90 sm:rotate-0 top-20 left-1/2 transform -translate-x-1/2 md:top-1/2 lg:top-1/2 md:left-20 lg:left-20">
+        <div className="z-40 absolute opacity-20 font-[dream] text-4xl sm:text-4xl md:text-6xl lg:text-6xl rotate-0 md:-rotate-90 lg:-rotate-90 sm:rotate-0 top-20 left-1/2 transform -translate-x-1/2 md:top-1/2 lg:top-1/2 md:left-20 lg:left-20">
           PORTFOLIO
         </div >
-        <Spline scene="https://prod.spline.design/q9KgjBw7qRiINVp5/scene.splinecode" />
+        <Spline scene="https://prod.spline.design/q9KgjBw7qRiINVp5/scene.splinecode" className='z-30'/>
 
+
+        <div className="absolute top-1/2 z-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
+          <p className='flex flex-col font-[dream] opacity-20 text-4xl sm:text-4xl md:text-6xl lg:text-6xl text-center justify-center items-center'>
+            <img src={loader} className='w-20 sm:w-20 md:w-40 lg:w-40 h-auto'></img>
+          Monde, <br></br>
+          Attends moi... <br></br>
+          J'arrive !</p>
+          </div>
         </div>
     </section>
   );
